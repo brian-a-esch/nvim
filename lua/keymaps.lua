b1e72dca9f3f3a5289950f141e7696a6f0a23250
Brian Esch <esch.bear@gmail.com>
2022-09-06 22:43:08 -0500

    More keymap controls

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,6 +2,18 @@ local keymap = vim.keymap.set
 -- Silent keymap option
 local opts = { silent = true }
 
+-- Somtimes on the internet there are normal vim re-mapping configs, 
+-- here's an exmplanation of them
+-- Commands                        Mode
+-----------                        ----
+-- nmap, nnoremap, nunmap          Normal mode
+-- imap, inoremap, iunmap          Insert and Replace mode
+-- vmap, vnoremap, vunmap          Visual and Select mode
+-- xmap, xnoremap, xunmap          Visual mode
+-- smap, snoremap, sunmap          Select mode
+-- cmap, cnoremap, cunmap          Command-line mode
+-- omap, onoremap, ounmap          Operator pending mode
+
 -- Sets Leader to space
 vim.g.mapleader = " "
 
@@ -12,8 +24,19 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
--- Resize with arrows
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize +2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)
+-- Resize with arrows. Test out with three or more tabs, thats
+-- when it gets fun. Regrettably vim seems to pin window sizes to the top
+-- left of the window. This makes resizing not feel right for the farthest
+-- right window, or the bottom window. Just use mouse?
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Set up tab movement to be like vimium, the chrome extension
+keymap("n", "J", ":bprevious<CR>", opts)
+keymap("n", "K", ":bnext<CR>", opts)
+
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
