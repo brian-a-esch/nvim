55a6f8878a956827de1a68bc3750dabc055e5c3e
Brian Esch <esch.bear@gmail.com>
2022-09-07 21:49:23 -0500

    Add tree sitter for syntax highlighting

--- lua/packages.lua
+++ lua/packages.lua
@@ -7,4 +7,5 @@ return require('packer').startup(function(use)
 	use { "catppuccin/nvim", as = "catppuccin" }
 	use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
 	use { "kyazdani42/nvim-tree.lua", requires = 'kyazdani42/nvim-web-devicons' }
+	use { 'nvim-treesitter/nvim-treesitter', run = function() require('nvim-treesitter.install').update({ with_sync = true }) end }
 end)
