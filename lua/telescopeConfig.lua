e01f00f3e4b6c495bc88b1140f35d11fc481d360
Brian Esch <esch.bear@gmail.com>
2022-09-10 12:43:47 -0500

    Configure telescope to be top down

--- lua/telescopeConfig.lua
+++ lua/telescopeConfig.lua
@@ -3,5 +3,9 @@ local telescope = require('telescope')
 telescope.setup{
   defaults = {
     prompt_prefix = " ",
-  }
+    layout_config = {
+      prompt_position = "top",
+    },
+    sorting_strategy = "ascending",
+  },
 }
