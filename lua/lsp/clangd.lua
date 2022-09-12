5da789695e04aadd443b1dfa54fd9eedf42b1dc9
Brian Esch <esch.bear@gmail.com>
2022-09-11 22:38:46 -0500

    Configure language server keymaps more to my liking

--- lua/lsp/clangd.lua
+++ lua/lsp/clangd.lua
@@ -8,20 +8,26 @@ local on_attach = function(client, bufnr)
 	local bufopts = { noremap=true, silent=true, buffer=bufnr }
 	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
 	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-	vim.keymap.set('n', 'H', vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', 'gh', vim.lsp.buf.hover, bufopts)
 	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-	vim.keymap.set('n', '<space>wl', function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, bufopts)
-	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+	-- This is what it is in clion, maybe change?
+	vim.keymap.set('i', '<C-p>', vim.lsp.buf.signature_help, bufopts)
+	-- These are workspace commands. Typically  I just start neovim in a project directory
+	-- so these are not needed to manually configure the lsp
+	--vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+	--vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+	--vim.keymap.set('n', '<space>wl', function()
+		--print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	--end, bufopts)
+	vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set('n', 'gc', vim.lsp.buf.rename, bufopts)
+	vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
 	-- Using telescope for this 
 	--vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-	vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+	
+	vim.keymap.set('n', 'gQ', vim.lsp.buf.formatting, bufopts)
+	-- range_formatting selects from last visual selection. Hence the escape at the beginning
+	vim.keymap.set('v', 'gq', '<ESC><cmd>lua vim.lsp.buf.range_formatting()<CR>', bufopts)
 end
 
 require('lspconfig')['clangd'].setup{
