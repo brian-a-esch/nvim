0f4f768e22c45f52e52fa0a168c5129c695f7f58
Brian Esch <brianesch@Brians-MacBook-Air.local>
2022-11-25 15:12:51 -0600

    Tweak current blame line config

--- lua/gitsignsConfig.lua
+++ lua/gitsignsConfig.lua
@@ -3,10 +3,10 @@ require('gitsigns').setup{
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-    delay = 1000,
+    delay = 500,
     ignore_whitespace = false,
   },
-  current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+  current_line_blame_formatter = '<author>, <abbrev_sha>, <author_time:%Y-%m-%d> - <summary>',
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
 
