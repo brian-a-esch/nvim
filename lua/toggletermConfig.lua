fec87db1dd619fa86c5257a218ad49cdde2a2874
Brian Esch <brianesch@Brians-MacBook-Air.local>
2022-11-25 15:13:39 -0600

    Add custon toggle terms for git

--- lua/toggletermConfig.lua
+++ lua/toggletermConfig.lua
@@ -9,3 +9,24 @@ require("toggleterm").setup{
   -- terminal and the main screen
   direction = "float",
 }
+
+local Terminal  = require('toggleterm.terminal').Terminal
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set("n", '<leader>gl', function()
+  local filename = vim.api.nvim_buf_get_name(0) -- 0 is current
+  local logTerminal = Terminal:new({
+    cmd = "tig --full-diff " .. filename
+  })
+  logTerminal:toggle()
+end, opts)
+
+vim.keymap.set("n", '<leader>gB', function()
+  local filename = vim.api.nvim_buf_get_name(0) -- 0 is current
+  local row, _ = unpack(vim.api.nvim_win_get_cursor(0)) -- 0 is current
+  local cmd = string.format('git blame --porcelain L %d,%d %s', row, row, filename)
+  cmd = cmd .. " | head -1 | awk '{print $1}'"
+  cmd = string.format('tig show $(%s)', cmd)
+  local logTerminal = Terminal:new({ cmd = cmd })
+  logTerminal:toggle()
+end, opts)
