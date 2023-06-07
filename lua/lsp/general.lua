03a5082e2f9437c02a1f403efc62b841df2eb376
Brian Esch <besch@drwholdings.com>
2023-06-07 15:38:26 -0500

    Fix semanticToken error
    This fixed a nil usage in neovim

--- lua/lsp/general.lua
+++ lua/lsp/general.lua
@@ -40,7 +40,7 @@ M.on_attach = function(client, bufnr)
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
   -- Disable this, I prefer the syntax highlighting provided by treesitter
-  client.server_capabilities.semanticTokensProvider = nil
+  client.server_capabilities.semanticTokensProvider = false
 
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
