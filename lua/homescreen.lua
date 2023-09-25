83108a61ed236a941526e451980a901eb6b25998
Brian Esch <esch.bear@gmail.com>
2023-09-24 21:20:23 -0500

    Format new code

--- lua/homescreen.lua
+++ lua/homescreen.lua
@@ -3,27 +3,27 @@ local dashboard = require("alpha.themes.dashboard")
 
 -- Set header
 dashboard.section.header.val = {
-    "                                                     ",
-    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-    "                                                     ",
+  "                                                     ",
+  "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+  "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+  "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+  "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+  "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+  "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+  "                                                     ",
 }
 
 -- Set menu
 dashboard.section.buttons.val = {
-    dashboard.button( "e", "  > New file", ":ene <BAR> startinsert <CR>"),
-    dashboard.button( "f", "  > Find file", ":Telescope find_files<CR>"),
-    dashboard.button( "g", "  > RipGrep", ":Telescope live_grep<CR>"),
-    -- Thse take advantage of the fact that the first buffer will be bufnr 1, and delete it
-    dashboard.button( "s", "  > Git Status", ":vert G | bd 1 <CR>"),
-    dashboard.button( "l", "  > Git Log", ":vert G log --stat --decorate | bd 1 <CR>"),
-    dashboard.button( "r", "  > Recent", ":Telescope oldfiles<CR>"),
-    dashboard.button( "c", "  > Config", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-    dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+  dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+  dashboard.button("f", "  > Find file", ":Telescope find_files<CR>"),
+  dashboard.button("g", "  > RipGrep", ":Telescope live_grep<CR>"),
+  -- Thse take advantage of the fact that the first buffer will be bufnr 1, and delete it
+  dashboard.button("s", "  > Git Status", ":vert G | bd 1 <CR>"),
+  dashboard.button("l", "  > Git Log", ":vert G log --stat --decorate | bd 1 <CR>"),
+  dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+  dashboard.button("c", "  > Config", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+  dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
 }
 
 alpha.setup(dashboard.opts)
