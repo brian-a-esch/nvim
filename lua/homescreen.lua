54cb5badd74d607347f8782a8df9167e787ef2d9
Brian Esch <besch@drwholdings.com>
2023-09-28 14:40:33 -0500

    Limit homescreen git log to 200 commits

--- lua/homescreen.lua
+++ lua/homescreen.lua
@@ -20,7 +20,7 @@ dashboard.section.buttons.val = {
   dashboard.button("g", "  > RipGrep", ":Telescope live_grep<CR>"),
   -- Thse take advantage of the fact that the first buffer will be bufnr 1, and delete it
   dashboard.button("s", "  > Git Status", ":vert G | bd 1 <CR>"),
-  dashboard.button("l", "  > Git Log", ":vert G log --stat --decorate | bd 1 <CR>"),
+  dashboard.button("l", "  > Git Log", ":vert G log --stat --decorate -200 | bd 1 <CR>"),
   dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
   dashboard.button("c", "  > Config", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
   dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
