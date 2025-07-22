7ea46cad79d5b7dd3fd996bf75c5c411664af9b2
Brian Esch <esch.bear@gmail.com>
2025-07-21 20:54:49 -0500

    Enable signature for blink

--- lua/completion.lua
+++ lua/completion.lua
@@ -24,6 +24,8 @@ blink.setup(
       nerd_font_variant = 'mono'
     },
 
+    signature = { enabled = true },
+
     -- (Default) Only show the documentation popup when manually triggered
     completion = { documentation = { auto_show = false } },
 
