0d739d127cf1882ca9a57754a551960f15323606
Brian Esch <esch.bear@gmail.com>
2025-07-21 22:08:56 -0500

    Add ripgrep to autocompletions

--- lua/completion.lua
+++ lua/completion.lua
@@ -32,7 +32,50 @@ blink.setup(
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = {
+        'lsp',
+        'path',
+        'snippets',
+        'buffer',
+        'ripgrep',
+      },
+      providers = {
+        ripgrep = {
+          module = "blink-ripgrep",
+          name = "Ripgrep",
+          opts = {
+            -- For many options, see `rg --help` for an exact description of
+            -- the values that ripgrep expects.
+            prefix_min_len = 3,
+            context_size = 5,
+            max_filesize = "1M",
+            project_root_marker = ".git",
+            -- Enable fallback to neovim cwd if project_root_marker is not
+            -- found. Default: `true`, which means to use the cwd.
+            project_root_fallback = true,
+            search_casing = "--ignore-case",
+            additional_rg_options = {
+              -- Ignore *.min.js files
+							glob = '!*.min.js',
+            },
+            -- Show debug information in `:messages` that can help in
+            -- diagnosing issues with the plugin.
+            debug = false,
+          },
+          -- (optional) customize how the results are displayed. Many options
+          -- are available - make sure your lua LSP is set up so you get
+          -- autocompletion help
+          transform_items = function(_, items)
+            for _, item in ipairs(items) do
+              -- example: append a description to easily distinguish rg results
+              item.labelDetails = {
+                description = "(rg)",
+              }
+            end
+            return items
+          end,
+        },
+      },
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
